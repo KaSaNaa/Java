24d73019223282a99e5c73665f5d72679080b3d6
Pazi2001 <pasindudeshitha123@gmail.com>
2022-08-17 00:55:58 +0530

    edited

---
+++ ConditionsAndBooleans/CondandBool.java
@@ -0,0 +1,13 @@
+package ConditionsAndBooleans;
+
+public class CondandBool {
+    public static void main(String[] args) {
+        int x =6;
+        int y= 7;
+        int z =8;
+        boolean compare;
+
+        System.out.println(x);
+        
+    }
+}
