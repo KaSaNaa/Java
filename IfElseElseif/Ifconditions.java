0f795e57c3c68723873098dc9fe699e3749a94db
Pazi2001 <pasindudeshitha123@gmail.com>
2022-08-23 10:26:42 +0530

    Added important notices

--- IfElseElseif/Ifconditions.java
+++ IfElseElseif/Ifconditions.java
@@ -2,6 +2,8 @@ package IfElseElseif;
 
 import java.util.Scanner;
 
+import javax.print.event.PrintEvent;
+
 public class Ifconditions {
     public static void main(String[] args) {
         System.out.println("Enter name: ");
@@ -14,6 +16,21 @@ public class Ifconditions {
             System.out.println("You entered Pasi.");
         } else {
             System.out.println("You didn't enter tim.");
+        }
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        sc.close(); // scannerVariable.close(); is used to close intput stream. if not IDE will show
+                    // a resource leak.
+
+        if (age > 20) {
+            System.out.print("You can ride bike.");
+        } else if (age > 15) {
+            System.out.print("You need to train more.");
+        } else if (age > 10) {
+            System.out.print("Grow up kid!");
+        } else {
+            System.out.print("Stay home please!");
         }
 
     }
---
+++ Questions/printHello/hello.java
@@ -0,0 +1,13 @@
+package Questions.printHello;
+
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.print("Enter your name: ");
+        Scanner NM = new Scanner(System.in);
+        String name = NM.next();
+        System.out.print("Hello ");
+        System.out.println(name);
+    }
+}
