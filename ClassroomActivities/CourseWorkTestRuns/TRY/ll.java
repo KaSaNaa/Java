3200526af2eb433308eabab4e4ed48563abc7726
Pazi2001 <pasindudeshitha123@gmail.com>
2022-10-26 01:45:38 +0530

    --

---
+++ ClassroomActivities/CourseWorkTestRuns/TRY/ll.java
@@ -0,0 +1,205 @@
+package ClassroomActivities.CourseWorkTestRuns.TRY;
+
+import java.util.Scanner;
+
+class Milk {
+    protected double price, totalCost;
+    protected char gradeOfMilk;
+    protected float qty, creamQty;
+
+    private void calculateTotalCost() {
+        totalCost = price * qty;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public char getGrade() {
+        return gradeOfMilk;
+    }
+
+    public float getQty() {
+        return qty;
+    }
+
+    public float getCreamQty() {
+        return creamQty;
+    }
+
+    public double getCost() {
+        calculateTotalCost();
+        return totalCost;
+    }
+
+    public void setQty(float qty) {
+        this.qty = qty;
+    }
+
+}
+
+class HighQualityMilk extends Milk {
+
+    public HighQualityMilk() {
+        price = 750;
+        gradeOfMilk = 'A';
+        creamQty = 2f;
+    }
+}
+
+class MidQualityMilk extends Milk {
+
+    public MidQualityMilk() {
+        price = 600;
+        gradeOfMilk = 'B';
+        creamQty = 1.5f;
+    }
+}
+
+class LowQualityMilk extends Milk {
+
+    public LowQualityMilk() {
+        price = 500;
+        gradeOfMilk = 'C';
+        creamQty = 1f;
+    }
+}
+
+class IceCream {
+    protected String flavor;
+    protected float qtyMade, sellingPrice;
+    protected double income = 0;
+    protected Milk milk;
+
+    private void calQty() {
+        qtyMade = milk.getQty() * milk.getCreamQty();
+    }
+
+    private void calIncome() {
+        calQty();
+        income = qtyMade * sellingPrice;
+    }
+
+    public void produceReport(Milk m) {
+        milk = m;
+        calIncome();
+        System.out.println("---- R e p o r t ----");
+        System.out.println("Total expenses:\t" + String.format("%.2f", milk.getCost()));
+        System.out.println("Total Income:\t" + String.format("%.2f", income));
+        System.out.println("Net Profit:\t" + String.format("%.2f", (income - milk.getCost())));
+
+    }
+
+}
+
+class Chocolate extends IceCream {
+
+    public Chocolate() {
+        flavor = "Chocolate";
+        sellingPrice = 1500f;
+    }
+}
+
+class Strawberry extends IceCream {
+
+    public Strawberry() {
+        flavor = "Strawberry";
+        sellingPrice = 1200f;
+    }
+}
+
+class Vanilla extends IceCream {
+
+    public Vanilla() {
+        flavor = "Vanilla";
+        sellingPrice = 1000f;
+    }
+}
+
+/*
+ * 
+ * Take milk quantity bought , grade of milk and the flavor of ice-cream as
+ * inputs and Calculate
+ * i. The total expenses of the company for that sale,
+ * ii. Total Income company obtained by that sale
+ * iii. Net profit from that sale.
+ */
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Milk milkObj = new Milk();
+        boolean flag = false;
+        String input = "";
+
+        // Validation
+        do {
+            System.out.println("Enter grade of milk: ");
+
+            switch (sc.next().toUpperCase()) {
+                case "A":
+                    milkObj = new HighQualityMilk();
+                    flag = true;
+                    break;
+
+                case "B":
+                    milkObj = new MidQualityMilk();
+                    flag = true;
+                    break;
+
+                case "C":
+                    milkObj = new LowQualityMilk();
+                    flag = true;
+                    break;
+
+                default:
+                    System.out.println("Invalid input. Please enter A, B or C");
+                    break;
+            }
+        } while (!flag);
+
+        do {
+            System.out.println("Enter quantity of milk bought: ");
+            input = sc.next();
+            try {
+                milkObj.setQty(Float.parseFloat(input));
+                break;
+            } catch (Exception e) {
+                System.out.println("Please enter a decimal number");
+            }
+        } while (true);
+
+        flag = false;
+
+        do {
+            System.out.println("Enter Flavour: ");
+            input = sc.next();
+            switch (input.toLowerCase()) {
+                case "chocolate":
+                    Chocolate chocolate = new Chocolate();
+                    chocolate.produceReport(milkObj);
+                    flag = true;
+                    break;
+
+                case "vanilla":
+                    Vanilla vanilla = new Vanilla();
+                    vanilla.produceReport(milkObj);
+                    flag = true;
+                    break;
+
+                case "strawberry":
+                    Strawberry strawberry = new Strawberry();
+                    strawberry.produceReport(milkObj);
+                    flag = true;
+                    break;
+
+                default:
+                    System.out.println("Please enter either Chocolate, Vanilla, or Strawberry");
+                    break;
+            }
+        } while (!flag);
+
+        sc.close();
+    }
+}
--- ClassroomActivities/CourseWorkTestRuns/TRY/milk.java
+++ ClassroomActivities/CourseWorkTestRuns/TRY/milk.java
@@ -1,40 +1,73 @@
 package ClassroomActivities.CourseWorkTestRuns.TRY;
 
-class Milk{
+import java.util.Scanner;
+
+class Milk {
     // buys milk from farmers
-    private float price, totalCost;
-    private String gradeOfMilk;
+    protected float price, ltrs, creamQty, totalCost;
+    protected char gradeOfMilk;
+
+
+    float setPrice() {
+        return price;
+    }
+
+    float setLtrs() {
+        return ltrs;
+    }
+
+    float getCreamQty() {
+        return creamQty;
+    }
 
     public float calculateTotalCost() {
         return totalCost;
     }
 
-    public float getprice() {
-        return price;
-    }
-
-    public String getMilkGrade() {
+    public char getMilkGrade() {
         return gradeOfMilk;
-        
+    }
+
+    
+}
+
+class HQmilk extends Milk {
+    /* 
+    needs to make an constructor in order to inherit instances from the parent
+    class
+    */
+    public HQmilk() {
+        price = 750; // sets price per litre for the specified milk type
+        creamQty = 2; // ice cream qty per milk ltr
+        gradeOfMilk = 'H';
     }
 }
 
-class HQmilk extends Milk{
-
+class MQmilk extends Milk {
+    public MQmilk() {
+        price = 600;
+        creamQty = 1.5f;
+        gradeOfMilk = 'M';
+    }
 }
 
-class MQmilk extends Milk{
-
-}
-
-class LQMilk extends Milk{
-
+class LQMilk extends Milk {
+    public LQMilk() {
+        price = 500;
+        creamQty = 1;
+        gradeOfMilk = 'L';
+    }
 }
 
 class IceCream {
-    private String flavour;
-    private float qty; // Qunatity Made
-    private float totInc;
+    protected char flavour;
+    protected float qty; // Qunatity Made
+    protected float pricePerLtr;
+    protected float totInc;
+
+    float setpricePerltr() {
+        return pricePerLtr;
+    }
 
     public float TotalIncome() {
         return totInc;
@@ -42,21 +75,46 @@ class IceCream {
 
 }
 
-class Chocolate extends IceCream{
+class Chocolate extends IceCream {
+    public Chocolate() {
+        pricePerLtr = 1500;
+        flavour = 'C';
+    }
 
 }
 
-class Vanilla extends IceCream{
+class Vanilla extends IceCream {
+    public Vanilla() {
+        pricePerLtr = 1000;
+        flavour = 'V';
+    }
 
 }
 
 class Strawberry extends IceCream {
-    
+    public Strawberry() {
+        pricePerLtr = 1200;
+        flavour = 'S';
+    }
 }
+/*
+ * Take milk quantity bought, grade of milk and the flavor of ice-cream as
+ * inputs and Calculate
+    *               i. The total expenses of the company for that sale,
+    *               ii. Total Income company obtained by that sale
+    *               iii. Net profit from that sale.
+ */
 
-class Main{
+class Main {
     public static void main(String[] args) {
         // calculate net income
+        Scanner sc = new Scanner(System.in);
+        Milk x = new Milk();
+
+        System.out.println("Enter milk type (High/Medium/Low) : ");
+        String type = sc.next();
+
+        sc.close();
     }
 
 }
